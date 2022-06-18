29c6e1ed7f0952cac03278453fc1df0a5a687c92
Andrei Borisov <clinri@ya.ru>
2022-06-18 22:25:01 +0300

    add layout bike part item for create and edit

---
+++ app/src/main/java/ru/netology/bikeparkstudenovsky/presentation/BikePartItemActivity.kt
@@ -0,0 +1,12 @@
+package ru.netology.bikeparkstudenovsky.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.bikeparkstudenovsky.R
+
+class BikePartItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bike_part_item)
+    }
+}
